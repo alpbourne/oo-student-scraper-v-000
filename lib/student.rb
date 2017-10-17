0f1a3da959129a587a077cacad28cd3ea6c05b06
alpbourne <alpbourne@gmail.com>
2017-10-17 19:42:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,8 +14,6 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       Student.new(student)
-      
-
   end
 
   def add_student_attributes(attributes_hash)
